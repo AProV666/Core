0c2efad31a39033c957980343e320607728ccef0
AProV666 <pro.andrei.v@gmail.com>
2022-09-09 12:01:19 +0300

    Homework 2 - Task 2 - changed

--- Homework2/src/Task2/Main.java
+++ Homework2/src/Task2/Main.java
@@ -5,61 +5,42 @@ Task2
     [3, 4, 2, 7], 10 -> [3, 7] - вывести пару именно в скобках, которые дают сумму - 10
  */
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.IntStream;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        final int[] inputArray = {3, 4, 2, 7};
-        final int countElements = 2;
-        final int targetSum = 10;
-
-        Optional<int[]> optional = getSumTerms(inputArray, countElements, targetSum);
-        optional.ifPresent(ints -> System.out.println(Arrays.toString(ints)));
-
+        int[] inputArray = {3, 4, 2, 7};
+        int targetSum = 10;
+        int[] resultArray = getSumTerms(inputArray, targetSum);
+        if (resultArray != null) {
+            System.out.println(Arrays.toString(resultArray));
+        }
         assertGetSumTerms();
     }
 
-    public static Optional<int[]> getSumTerms(int[] inputArray, int countElements, int targetSum) {
-        return getCombinationsList(inputArray, countElements)
-                .stream()
-                .filter(array -> IntStream.of(array).sum() == targetSum)
-                .findFirst();
-    }
-
-    public static List<int[]> getCombinationsList(int[] inputArray, int countElements) {
-        int inputArrayLength = inputArray.length;
-        int[] emptyArray = new int[countElements];
-        List<int[]> pairList = new ArrayList<>();
-        combinations(pairList, inputArray, inputArrayLength, countElements, 0, emptyArray, 0);
-        return pairList;
-    }
-
-    public static void combinations(List<int[]> pairList, int[] inputArray, int inputArrayLength, int countElements,
-                                    int arrayIndex, int[] emptyArray, int x) {
-        if (arrayIndex == countElements) {
-            int[] arr = new int[countElements];
-            System.arraycopy(emptyArray, 0, arr, 0, countElements);
-            pairList.add(arr);
-            return;
+    public static int[] getSumTerms(int[] array, int targetSum) {
+        if (array == null || array.length < 2)
+            return null;
+        Arrays.sort(array);
+        int firstIndex = 0;
+        int lastIndex = array.length - 1;
+        while (firstIndex < lastIndex) {
+            if (array[firstIndex] + array[lastIndex] == targetSum) {
+                return new int[]{array[firstIndex], array[lastIndex]};
+            } else if (array[firstIndex] + array[lastIndex] > targetSum) {
+                lastIndex--;
+            } else {
+                firstIndex++;
+            }
         }
-        if (x >= inputArrayLength)
-            return;
-        emptyArray[arrayIndex] = inputArray[x];
-        combinations(pairList, inputArray, inputArrayLength, countElements, arrayIndex + 1, emptyArray, x + 1);
-        combinations(pairList, inputArray, inputArrayLength, countElements, arrayIndex, emptyArray, x + 1);
+        return null;
     }
 
     public static void assertGetSumTerms() {
         final int[] array = {5, 7, 4, 9, 1};
         final int[] templateArray = {4, 9};
-        final int countElements = 2;
         final int targetSum = 13;
-        final int[] resultArray = getSumTerms(array, countElements, targetSum).orElse(null);
+        final int[] resultArray = getSumTerms(array, targetSum);
         assert Arrays.equals(templateArray, resultArray) : "Incorrect array!";
     }
 }
